22e8bdfddc241bf2592345d94316d6020dcf68ea
YaqiangCao <caoyaqiang0410@gmail.com>
2026-01-30 15:39:36 -0500

    updates

--- Results/1.RNA-seq/2.PreleukemiaDEGs/2.listGos/1.go/get.py
+++
@@ -1,9 +0,0 @@
-import os
-from glob import glob
-
-fs = glob("../../6.refinePreLeukemiaDEGs/*.list")
-for f in fs:
-    n = f.split("/")[-1].split(".")[0]
-    cmd = "findGO.pl %s mouse %s -cpu 10 &"%(f,n)
-    print(cmd)
-    os.system(cmd)
