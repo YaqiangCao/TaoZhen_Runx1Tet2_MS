2b05dc06a5b73f4cbe437b2a725c8a318c85e9a4
YaqiangCao <caoyaqiang0410@gmail.com>
2026-03-23 10:07:20 -0400

    updates

---
+++ Results/1.RNA-seq/5.othersComparedToControlUpdates/5.control_preleukemias_GOs/1.go/get.py
@@ -0,0 +1,10 @@
+import os
+from glob import glob
+
+fs = glob("../../17.preleukemia_controlvsothers_revise/c*.txt")
+
+for f in fs:
+    n = f.split("/")[-1].split(".")[0]
+    cmd = "findGO.pl %s mouse %s -cpu 10 &"%(f,n)
+    print(cmd)
+    os.system(cmd)
